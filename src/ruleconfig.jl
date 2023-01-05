010f389aa826f17896d561063bba8859736115f0
marius <mariusmillea@gmail.com>
2023-01-05 03:00:08 -0800

    reset Zygote context cache before each call

--- src/ruleconfig.jl
+++ src/ruleconfig.jl
@@ -8,8 +8,10 @@ struct ReverseRuleConfigBackend{RC<:ChainRulesCore.RuleConfig{>:ChainRulesCore.H
 end
 
 AD.@primitive function pullback_function(ab::ReverseRuleConfigBackend, f, xs...)
-    _, back = ChainRulesCore.rrule_via_ad(ab.ruleconfig, f, xs...)
+    _, back = ChainRulesCore.rrule_via_ad(empty_cache!(ab.ruleconfig), f, xs...)
     pullback(vs) = Base.tail(back(vs))
     pullback(vs::Tuple{Any}) = Base.tail(back(first(vs)))
     return pullback
 end
+
+empty_cache!(ruleconfig::ChainRulesCore.RuleConfig) = ruleconfig
